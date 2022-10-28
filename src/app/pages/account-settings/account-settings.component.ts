60c83763533825788d17ec3ac1e7274b482565ab
carlos <carlos.melgarejo.solorzano@hotmail.com>
2022-10-28 11:39:28 -0500

    segundocommit

--- src/app/pages/account-settings/account-settings.component.ts
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -8,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountSettingsComponent implements OnInit {
 
-  constructor() { }
+  public colorMain = document.querySelector('#theme')
 
-  ngOnInit(): void {
+  constructor() {
+    const colorInicial =  localStorage.getItem('color-theme') || './assets/css/colors/default-dark.css'
+    this.colorMain?.setAttribute('href',colorInicial)
+
+
+
   }
 
+  ngOnInit() {
+    this.currentColor()
+  }
+
+
+  cambiarColor(color : string){
+    const colorDinamico = `./assets/css/colors/${color}.css`
+    this.colorMain?.setAttribute('href',colorDinamico);
+
+    localStorage.setItem('color-theme',colorDinamico)
+
+
+
+    this.currentColor()
+  }
+
+  currentColor(){
+      const selectorAll = document.querySelectorAll('.selector')
+
+      selectorAll.forEach(selector => {
+        selector.classList.remove('working');
+
+        const themeSimple  = selector.getAttribute('data-theme')
+        const getColorMain = this.colorMain?.getAttribute('href')
+        const urlCurrent = `./assets/css/colors/${themeSimple}.css`
+
+
+        if(urlCurrent === getColorMain){
+          selector.classList.add('working')
+          console.log(selector)
+        }
+
+      })
+
+
+  }
 }
